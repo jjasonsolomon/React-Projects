b2424e6520bed6f83673d7b62483451aee4528d8
Jason Solomon <34937880+jjasonsolomon@users.noreply.github.com>
2021-10-17 16:56:18 +0530

    Working with Filter List

--- src/components/Expense/Expenses.js
+++ src/components/Expense/Expenses.js
@@ -6,30 +6,43 @@ import { useState } from 'react';
 const Expenses = (props) =>
 {
 
+
   const [selectedYear,setSelectedYear] = useState('2021');
 
    const expenseFilterHandler=(filteredYear)=>{
+    setSelectedYear(filteredYear);
     console.log(filteredYear)
-    setSelectedYear(filteredYear)
+  
+    }
 
-  }
+    const filteredExpenses = props.expenses.filter(expense => 
+    expense.date.getFullYear().toString()===selectedYear)
+   
+    let expenseContent = <p>No Items Found.</p>;
 
+    if(filteredExpenses.length>0)
+    {
+      expenseContent=filteredExpenses.map(expenses =>
+      
+        
+        <ExpenseItems key={expenses.id} title={expenses.title} date={expenses.date} amount={expenses.amount} > </ExpenseItems>
+        
+        )
+    }
     return(
 
         
   <Card className="expenses">
 
     <div>
-      
-      
     
+
     <ExpensesFilter selected={selectedYear} onExpenseFilterData={expenseFilterHandler}/>
-    {  
-     props.expenses.map(expenses =>
-    <ExpenseItems key={expenses.id} title={expenses.title} date={expenses.date} amount={expenses.amount} > </ExpenseItems>
-    )}
     
    </div>
+   {expenseContent} 
+
+
    </Card>
     );
 
